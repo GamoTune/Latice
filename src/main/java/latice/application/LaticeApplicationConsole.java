39282a8bb9de2aff6bda6a7170cf01a592a1239f
Valentin Bonnelie <val.bonnelie@gmail.com>
2025-04-07 16:50:24 +0200

    Ajout du collaborateur Valentin sur le message de bienvenue du projet de SAE

--- src/main/java/latice/application/LaticeApplicationConsole.java
+++ src/main/java/latice/application/LaticeApplicationConsole.java
@@ -6,7 +6,8 @@ public class LaticeApplicationConsole {
 		System.out.println("------------------------------------------------------");
 		System.out.println("--  Bienvenue dans notre magnifique jeu de latice ! --");
 		System.out.println("--  développé par Loïs                              --");
-		System.out.println("--  développé par Malik                             --");
+		System.out.println("--  et par Malik                                    --");
+		System.out.println("--  et par Valentin                                 --");
 		System.out.println("------------------------------------------------------");
 
 	}
