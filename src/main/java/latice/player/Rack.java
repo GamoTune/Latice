38f0a28c10c434623806599f1534d40a67a8c757
Val24430 <val.bonnelie@gmail.com>
2025-04-17 10:30:31 +0200

    implement class Rack
    Creation of the Rack class

--- src/main/java/latice/player/Rack.java
+++ src/main/java/latice/player/Rack.java
@@ -1,5 +1,19 @@
 package latice.player;
 
+import java.util.List;
+
+import latice.tile.Tile;
+
 public class Rack {
+    private List<Tile> tiles;
+
+    public Rack(List tiles) {
+        this.tiles = tiles;
+    }
+
+    public List get_Tiles() {
+        return tiles;
+    }
+
 
 }
