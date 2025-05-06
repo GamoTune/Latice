c64364a7f883ef68c7c91c7894a62cf27f6eaacb
Abdel-Malik Moussaoui <amalik.moussaoui@gmail.com>
2025-05-06 13:53:40 +0200

    Complete implementation of Position class

--- src/main/java/latice/cell/Position.java
+++ src/main/java/latice/cell/Position.java
@@ -1,5 +1,19 @@
 package latice.cell;
 
-public class Position {
-	//TODO: Implement the position logic
+public abstract class Position {
+    private int x;
+    private int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int x() {
+        return x;
+    }
+
+    public int y() {
+        return y;
+    }
 }
