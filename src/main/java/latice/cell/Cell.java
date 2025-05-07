e68131dc04098f512e8bb926b7dd215c81b8afbb
Arthur Labregere <arthur.labregere@gmail.com>
2025-05-07 15:21:26 +0200

    Implements GameBoard and Cell classes
    Initializes the game board with cells of type SUN, MOON and NORMAL.
    Adds Cell class with type and tile properties.
    Adds CellType enum with SUN, MOON and NORMAL types.
    Implements Position class to represent cell coordinates.

--- src/main/java/latice/cell/Cell.java
+++ src/main/java/latice/cell/Cell.java
@@ -4,8 +4,21 @@ import latice.tile.Tile;
 
 public class Cell {
 	private final CellType type;
+	private Tile tile;
 	
 	public Cell(CellType type) {
 		this.type = type;
+	}
+	
+	public CellType getType() {
+		return type;
+	}
+	
+	public void setTile(Tile tile) {
+		this.tile = tile;
+	}
+	
+	public Tile getTile() {
+		return tile;
 	}
 }
